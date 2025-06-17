17effd19133c3224168e0267e3d2884f03b99bb7
DekiruST <jose.5.laurel@gmail.com>
2025-06-17 13:31:16 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders Civilization VII title', () => {
   render(<App />);
-  const titleElement = screen.getByText(/Civilization VII/i);
+  
+
+  const titleElement = screen.getByRole('heading', { 
+    name: /Civilization VII/i,
+    level: 1
+  });
+  
   expect(titleElement).toBeInTheDocument();
 });
